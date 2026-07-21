a00b4f7682c9a869767e945fff7ac976d355024f
MintCookies04 <fatehkirmani123@gmail.com>
2026-07-21 21:51:48 +0500

    Add fixed singleton id to LaborProjectionSettings and EstimateDefaults

---
+++ prisma/migrations/20260721165050_laborprojectionsettings_estimatedefaults_singleton_id/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "EstimateDefaults" ALTER COLUMN "id" SET DEFAULT 'singleton';
+
+-- AlterTable
+ALTER TABLE "LaborProjectionSettings" ALTER COLUMN "id" SET DEFAULT 'singleton';
